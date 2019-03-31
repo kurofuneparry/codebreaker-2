b8326d9e55baa5b743f51d70e1f4c0e061f80fa1
kurofuneparry <kurofuneparry@gmail.com>
2019-03-30 20:26:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ let choose = function(choices) { // Picks a random element from choices
 	return choices[Math.floor(Math.random() * choices.length)];
 }
 while (secret.length < length) {
-    secret[secret.length] = choose(colors);
+	secret[secret.length] = choose(colors);
 }
 
 // Makes a dot
 function makeDot(color=colors[0], htmlClass='dot') {
 	let dot = document.createElement('span');
-    dot.style.backgroundColor = color;
+	dot.style.backgroundColor = color;
 	dot.className = htmlClass;
 	return dot;
 }
@@ -35,8 +35,8 @@ for (let i=0; i<length; i++) {
 // Clears the current guess
 function clear() {
 	for (let i=0; i<guessDiv.children.length; i++) {
-	    guessDiv.children[i].style.backgroundColor = 'white';
-	    guessDiv.children[i].className = 'empty';
+		guessDiv.children[i].style.backgroundColor = 'white';
+		guessDiv.children[i].className = 'empty';
 	}
 	responseDiv.innerText = '';
 }
@@ -51,11 +51,12 @@ menu.appendChild(clearButton);
 function addColor(color) {
 	// The first 'empty' dot is colored
 	for (let i=0; i<guessDiv.children.length; i++) {
-	    if (guessDiv.children[i].className == 'empty') {
-	        guessDiv.children[i].style.backgroundColor = color;
-	        guessDiv.children[i].className = 'dot';
-	        break; // No 'empty' dot? Then nothing happens in this loop
-	    }
+		// If there are no 'empty' dots, then nothing happens
+		if (guessDiv.children[i].className == 'empty') {
+	 		guessDiv.children[i].style.backgroundColor = color;
+	        	guessDiv.children[i].className = 'dot';
+	        	break; // No 'empty' dot? Then nothing happens in this loop
+	    	}
 	}
 }
 
